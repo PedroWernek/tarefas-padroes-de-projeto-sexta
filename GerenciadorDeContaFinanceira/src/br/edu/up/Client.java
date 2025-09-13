1aa5166d558a7f5c1fc3db25c9bef70f8732253d
PedroWernek <pedrowcnetto@gmail.com>
2025-09-13 09:53:39 -0300

    finishing Client

--- GerenciadorDeContaFinanceira/src/br/edu/up/Client.java
+++ GerenciadorDeContaFinanceira/src/br/edu/up/Client.java
@@ -1,15 +1,22 @@
 package br.edu.up;
 
+import java.util.List;
+
 public class Client {
 
     public String name;
-    public AccountManager account;
+    public static AccountManager account;
+    public String accountId;
 
-    public void depositar(String accountId, double value) {
-        account.depositar(accountId, value);
+    public static void acessarConta(List<Account> accounts) {
+        Client.account = AccountManager.getInstance(accounts);
     }
 
-    public void sacar(String accountId, double value) {
-        account.depositar(accountId, value);
+    public void depositar(double value) {
+        account.depositar(this.accountId, value);
+    }
+
+    public void sacar(double value) {
+        account.sacar(this.accountId, value);
     }
 }
